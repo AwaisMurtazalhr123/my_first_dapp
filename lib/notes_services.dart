66b87ef4887209887aaaa2ab0533158e05d2ffad
AwaisMurtazalhr123 <awais14me123@gmail.com>
2022-07-23 16:47:00 +0500

    Test app

---
+++ lib/notes_services.dart
@@ -0,0 +1,127 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:my_first_dapp/note.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:http/http.dart' as http;
+
+class NotesServices extends ChangeNotifier {
+  List<Note> notes = [];
+  final String _rpcUri =
+      Platform.isAndroid ? 'http://10.0.0.1:7545' : 'http://127.0.0.1:7545';
+  final String _wsUrl =
+      Platform.isAndroid ? 'http://10.0.0.1:7545' : 'ws://127.0.0.1:7545';
+  bool isLoading = true;
+
+  final String _privatekey =
+      "4710f94e39e4d9c67b978fe2262d187bfe847d2bb9dfcaed0df44500fe6d96a9";
+
+  late Web3Client _web3client;
+
+  NotesServices() {
+    init();
+  }
+  Future<void> init() async {
+    _web3client = Web3Client(
+      _rpcUri,
+      http.Client(),
+      socketConnector: () {
+        return IOWebSocketChannel.connect(_wsUrl).cast<String>();
+      },
+    );
+    await getABI();
+    await getCredentials();
+    getDeployedContract();
+  }
+
+  late ContractAbi _abiCode;
+  late EthereumAddress _contractAddress;
+  Future<void> getABI() async {
+    String abiFile =
+        await rootBundle.loadString('builid/contracts/NotesContract.json');
+    var jsonABI = jsonDecode(abiFile);
+    _abiCode =
+        ContractAbi.fromJson(jsonEncode(jsonABI['abi']), "NotesContract");
+    _contractAddress =
+        EthereumAddress.fromHex(jsonABI["networks"]["5777"]["address"]);
+  }
+
+  late EthPrivateKey _creds;
+  Future<void> getCredentials() async {
+    _creds = EthPrivateKey.fromHex(_privatekey);
+  }
+
+  late DeployedContract _deployedContract;
+  late ContractFunction _createNote;
+  late ContractFunction _deleteNote;
+  late ContractFunction _notes;
+  late ContractFunction _noteCount;
+
+  Future<void> getDeployedContract() async {
+    _deployedContract = DeployedContract(_abiCode, _contractAddress);
+    _createNote = _deployedContract.function('createNote');
+    _deleteNote = _deployedContract.function('deleteNote');
+    _notes = _deployedContract.function('notes');
+    _noteCount = _deployedContract.function('noteCount');
+    await fetchNotes();
+  }
+
+  Future<void> fetchNotes() async {
+    List totalTaskList = await _web3client.call(
+      contract: _deployedContract,
+      function: _noteCount,
+      params: [],
+    );
+
+    int totalTaskLen = totalTaskList[0].toInt();
+    notes.clear();
+    for (var i = 0; i < totalTaskLen; i++) {
+      var temp = await _web3client.call(
+          contract: _deployedContract,
+          function: _notes,
+          params: [BigInt.from(i)]);
+      if (temp[1] != "") {
+        notes.add(
+          Note(
+            id: (temp[0] as BigInt).toInt(),
+            title: temp[1],
+            description: temp[2],
+          ),
+        );
+      }
+    }
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> addNote(String title, String description) async {
+    await _web3client.sendTransaction(
+      _creds,
+      Transaction.callContract(
+        contract: _deployedContract,
+        function: _createNote,
+        parameters: [title, description],
+      ),
+    );
+    isLoading = true;
+    notifyListeners();
+    fetchNotes();
+  }
+
+  Future<void> deleteNote(int id) async {
+    await _web3client.sendTransaction(
+      _creds,
+      Transaction.callContract(
+        contract: _deployedContract,
+        function: _deleteNote,
+        parameters: [BigInt.from(id)],
+      ),
+    );
+    isLoading = true;
+    notifyListeners();
+    fetchNotes();
+  }
+}
